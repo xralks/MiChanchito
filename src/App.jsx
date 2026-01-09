c4afc210a2c575bed095b3c6dc7301382dcbcc50
xralks <ramiroandres.sc@gmail.com>
2026-01-08 22:02:15 -0300

    Primer Commit: Subida de archivos base proyecto

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import AppRoutes from './routes/AppRoutes'
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <main className="main-container">
+        <AppRoutes />
+      </main>
+    </>
+  )
+}
+
+export default App
