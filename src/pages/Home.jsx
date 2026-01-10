105a9ef464b3428d237a141a0affee4594a0774c
xralks <ramiroandres.sc@gmail.com>
2026-01-10 19:01:37 -0300

    Feat:Se agrega nueva sección de footer

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,7 @@
 import HeroHome from "../components/HeroHome/HeroHome";
 import FuncionalidadesHome from "../components/FuncionalidadesHome/FuncionalidadesHome";
 import SeguridadConfHome from "../components/SeguridadConfHome/SeguridadConfHome";
+import FooterHome from "../components/FooterHome/FooterHome";
 
 const Inicio = () => {
   return (
@@ -8,6 +9,7 @@ const Inicio = () => {
       <HeroHome />
       <FuncionalidadesHome/>
       <SeguridadConfHome/>
+      <FooterHome/>
     </div>
   );
 };
