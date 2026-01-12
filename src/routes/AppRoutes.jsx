6526cd26c02972cffc94b3ef82439bc270b258e2
xralks <ramiroandres.sc@gmail.com>
2026-01-12 00:18:20 -0300

    Feat: se crea la pagina de login junto con archivo css para todas las paginas de autentificación llamado auth.css

--- src/routes/AppRoutes.jsx
+++ src/routes/AppRoutes.jsx
@@ -1,12 +1,14 @@
 import { Routes, Route } from 'react-router-dom'
 import Home from '../pages/Home.jsx'
 import Nosotros from '../pages/Nosotros.jsx'
+import Login from '../pages/Login.jsx'
 
 export default function AppRoutes() {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/Nosotros" element={<Nosotros />} />
+      <Route path="/Login" element={<Login />} />
     </Routes>
   )
 }
