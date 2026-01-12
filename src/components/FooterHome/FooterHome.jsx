4cc08f2aea2c0682d2b6eb3cef3d72a4baca8dd9
xralks <ramiroandres.sc@gmail.com>
2026-01-12 01:16:51 -0300

    Update: cambio de ruta a Registro en boton FooterHome

--- src/components/FooterHome/FooterHome.jsx
+++ src/components/FooterHome/FooterHome.jsx
@@ -5,7 +5,7 @@ const FooterHome = () => {
         <section className="footerHome">
             <h1><span className="resaltarTexto"> Da el primer paso hacia tus metas financieras.</span></h1>
             <p>Únete a MiChanchito hoy y transforma tu gestión financiera con la potencia de la inteligencia artificial.</p>
-            <a href="/Inicio-Sesion" className="boton-primario">Crear cuenta gratuita</a>
+            <a href="/Registro" className="boton-primario">Crear cuenta gratuita</a>
         </section>
     );
 }
